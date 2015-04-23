b6e39e2a15a2be0a0e572e39b28c27cd70f2e83c
djry35 <djry35@mail.missouri.edu>
2015-04-23 18:31:52 -0500

    search beta finished

--- php/DB_Search.php
+++ php/DB_Search.php
@@ -1,4 +1,6 @@
 <?php
+/*
+
 //Need to include functions
 require 'functions.php';
 //Check if pawprint or last name, first name search
@@ -34,4 +36,88 @@ if($conn) {
    //echo the array of arrays as a json object
    echo json_encode($all_stu, JSON_UNESCAPED_SLASHES);
 }
+
+*/
+function search($dbconn, $string, $type)
+{
+    $returnResults = array();
+
+    if(strcmp($type, "pawprint") == 0)
+    {
+        $test = strtok($string, " ");
+        if(strcmp($test, $string) != 0)
+        {
+            return NULL;
+        }
+        else
+        {
+            pg_prepare($dbconn, "pawprint", "SELECT * FROM applicant WHERE sso LIKE $1");
+            $SR = pg_execute($dbconn, "pawprint", array($test));
+            $line = pg_fetch_array($SR, null, PGSQL_ASSOC);
+            $returnResults[0] = $line;
+            $returnResults[0] = json_encode($returnResults[0]);
+        }
+    }
+    else
+    {
+        $test = strtok($string, " ,\n");
+        if(strcmp($test, $string) == 0)
+        {
+            pg_prepare($dbconn, "fname", "SELECT * FROM applicant WHERE fname LIKE $1");
+            $SR = pg_execute($dbconn, "fname", array($test));
+            $i = 0;
+            while($line = pg_fetch_array($SR, null, PGSQL_ASSOC))
+            {
+                $returnResults[$i] = $line;
+                $returnResults[$i] = json_encode($returnResults[$i]);
+                $i++;
+            }
+            pg_prepare($dbconn, "lname", "SELECT * FROM applicant WHERE lname LIKE $1");
+            $SR = pg_execute($dbconn, "lname", array($test));
+            $i = 0;
+            while($line = pg_fetch_array($SR, null, PGSQL_ASSOC))
+            {
+                $returnResults[$i++] = $line;
+                $returnResults[$i] = json_encode($returnResults[$i]);
+                $i++;
+            }
+        }
+        else
+        {
+            $second = strtok(" \n");
+            $third = strtok(" ");
+            if($third == true)
+            {
+                return NULL;
+            }
+            else
+            {
+                pg_prepare($dbconn, "fname", "SELECT * FROM applicant WHERE fname LIKE $1 AND lname LIKE $2");
+                $SR = pg_execute($dbconn, "fname", array($test, $second));
+                $i = 0;
+                while($line = pg_fetch_array($SR, null, PGSQL_ASSOC))
+                {
+                    $returnResults[$i] = $line;
+                    $returnResults[$i] = json_encode($returnResults[$i]);
+                    $i++;
+                }
+                pg_prepare($dbconn, "lname", "SELECT * FROM applicant WHERE lname LIKE $1 AND fname LIKE $2");
+                $SR = pg_execute($dbconn, "lname", array($test, $second));
+                $i = 0;
+                while($line = pg_fetch_array($SR, null, PGSQL_ASSOC))
+                {
+                    $returnResults[$i] = $line;
+                    $returnResults[$i] = json_encode($returnResults[$i]);
+                    $i++;
+                }
+            }
+        }
+    }
+
+
+}
+
+
+
+
 ?>
