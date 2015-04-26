d6db4d8703bb7056eaf2758f35a2254ade073d49
Devin Petersohn <dpvx8@mail.missouri.edu>
2015-04-26 17:20:46 -0500

    rank added

--- php/functions.php
+++ php/functions.php
@@ -12,12 +12,6 @@ function db_connect() {
     return $DBconn;
 }
 
-function addRank($dbconn, $applicant, $rank)
-{
-    pg_prepare($dbconn, "addition", "UPDATE applicant SET ranking = $1 WHERE sso LIKE $applicant");
-    pg_execute($dbconn, "addition", array(htmlspecialchars($rank)));
-}
-
 function offerPosition($dbconn, $applicant, $course, $section)
 {
     pg_prepare($dbconn, "position", "INSERT INTO applicant_offer_received VALUES($1, $2, $3, NULL, NULL)");
