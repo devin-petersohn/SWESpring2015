ac6ce87108259226745ce0d4a03dcc2445967503
zdc9pd <zdc9pd@mail.missouri.edu>
2015-04-27 15:02:12 -0500

    added add_comment.php

---
+++ php/add_comment.php
@@ -0,0 +1,27 @@
+<?php
+require 'functions.php';
+
+$sso = $_POST['sso'];
+$course_id = $_POST['course_id'];
+//$dates_taught = $_POST['dates-taught'];
+$instructor_sso = $_SESSION['username'];
+$comment = $_POST['stu_comment'];
+
+/* $qry = 'INSERT INTO applicant_comments (sso, course_id, dates_taught, instructor_sso, comment)
+ VALUES ('.$sso.','. $course_id.','. $dates_taught.','. $instructor_sso.','. $comment.')
+'; */
+$qry = 'INSERT INTO applicant_comments (sso, course_id, instructor_sso, comment)
+            VALUES ('.$sso.','. $course_id.','. $instructor_sso.','. $comment.')
+           ';
+
+pg_prepare($conn, "add_comment", $qry);
+if(pg_execute($conn, "add_comment", array())) {
+    echo "Comment added.\n";
+    echo"<br> Return to <a href = 'swespring2015/module/Comments/view/comments/comments/index.phtml'>comments page</a>\n";
+}
+else {
+    echo "Delete Failed <br />";
+    echo"<br> Return to <a href = 'swespring2015/module/Comments/view/comments/comments/index.phtml'>comment page</a>\n";
+}
+
+?>
