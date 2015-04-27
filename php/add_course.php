711623140486351eff195fb2e780941aef4f59f9
Devin Petersohn <dpvx8@mail.missouri.edu>
2015-04-27 18:43:13 -0500

    change instead of post

--- php/add_course.php
+++ php/add_course.php
@@ -3,8 +3,9 @@ include 'functions.php';
 
 $db = db_connect();
 
-pg_prepare($db, "q1", "INSERT INTO Course (course_id, section, slots_available) VALUES ($1,$2,$3)");
-pg_execute($db, "q1", array($_POST['cid'], $_POST['section'], $_POST['slots']));
-
+function addCourse($course_id, $section, $slots) {
+    pg_prepare($db, "q1", "INSERT INTO Course (course_id, section, slots_available) VALUES ($1,$2,$3)");
+    pg_execute($db, "q1", array($course_id, $section, $slots));
+}
 
 ?>
