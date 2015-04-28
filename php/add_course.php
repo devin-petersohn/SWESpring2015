ae6d534529ee50f8679c31c6c8b32665a88524c7
Madelyn Petersohn <mcs526@mail.missouri.edu>
2015-04-27 21:30:59 -0500

    change db_connect function

--- php/add_course.php
+++ php/add_course.php
@@ -4,6 +4,7 @@ include 'functions.php';
 $db = db_connect();
 
 function addCourse($course_id, $section, $slots) {
+    $db = db_connect();
     pg_prepare($db, "q1", "INSERT INTO Course (course_id, section, slots_available) VALUES ($1,$2,$3)");
     pg_execute($db, "q1", array($course_id, $section, $slots));
 }
