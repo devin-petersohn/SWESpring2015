848cf2744af880038e2b6da2d7e022491f6eb982
xltz6 <xltz6@mail.missouri.edu>
2015-05-01 04:37:06 +0800

    change add_comment.php

--- php/add_comment.php
+++ php/add_comment.php
@@ -7,6 +7,8 @@ if($conn) {
     $dates_taught = $_POST['dates-taught'];
     $instructor_sso = $_SESSION['username'];
     $comment = $_POST['stu_comment'];
+    //echo $_POST['course_id'];
+    //echo $_SESSION['username'];
     
      $qry = 'INSERT INTO applicant_comments (sso, course_id, dates_taught, instructor_sso, comment)
      VALUES ('.$sso.','. $course_id.','. $dates_taught.','. $instructor_sso.','. $comment.')
@@ -17,14 +19,15 @@ if($conn) {
     
     pg_prepare($conn, "add_comment", $qry);
     
-    echo json_encode(
-        array(
+    $arr = array(
             "success" => "1",
             "course_id" => htmlentities($course_id), 
             "instructor_sso" => htmlentities($instructor_sso),
             "comment" => htmlentities($comment)
-        )
-    );
+            );
+    
+    echo json_encode($arr);
+        
     
 }
 ?>
