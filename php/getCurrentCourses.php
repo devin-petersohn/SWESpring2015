315494890dce7fede76e0e1520316c6294c7b6fa
Devin Petersohn <dpvx8@mail.missouri.edu>
2015-05-01 17:15:33 -0500

    Updated application backend to improve functionality.

--- php/getCurrentCourses.php
+++ php/getCurrentCourses.php
@@ -6,7 +6,7 @@
 	$course = pg_query($db, "SELECT course_id FROM course WHERE slots_available > 0;");
 		echo "<select name='selectCourses' id='selection' value='selection' >";
 		while ($printedCourse = pg_fetch_row($course)) {
-		    echo "<option value = '".$i."'>".$printedCourse[0]."</option>";
+		    echo "<option value = '".$printedCourse[0]."'>".$printedCourse[0]."</option>";
 		}
 		echo "<\select>"
 
