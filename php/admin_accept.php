d6677216fc7fe928eb96b848a6bff6c0c69ab465
Devin Petersohn <dpvx8@mail.missouri.edu>
2015-05-01 18:09:14 -0500

    Created new files for backend to accept/reject students and student offers

---
+++ php/admin_accept.php
@@ -0,0 +1,13 @@
+<?php
+	session_start();
+	include 'functions.php';
+
+	function acceptance($choice) {
+		$db = db_connect();
+
+		$sso = $_SESSION['username'];
+
+		pg_prepare($db, 'UPDATE applicant_offer_received SET assigned_to_course = $1 WHERE sso = $2');
+		pg_execute($db, "q1", array($choice, $sso));
+	}
+?>
