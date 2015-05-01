7cd0368e18c11e463c11fe1a4f5cbd1630c9aa51
djry35 <djry35@mail.missouri.edu>
2015-05-01 16:49:13 -0500

    hopefully this works

--- php/test2.php
+++ php/test2.php
@@ -1,4 +1,16 @@
 <?php
-//Daniel test of Zend Studio issues
+use Zend\Crypt\BlockCipher;
+use Zend\Crypt\Symmetric\Mcrypt;
+
+$filter = new Zend\Filter\Encrypt(array('adapter' => 'BlockCipher'));
+$filter->setKey('asfd');
+
+$encrypt = $filter->filter('this is a test');
+echo $encrypt;
+
+$filter2 = new Zend\Filter\Decrypt();
+$filter2->setKey('asfd');
+$filter2 = $filter2->filter($filter);
+echo $filter2;
 
 ?>
