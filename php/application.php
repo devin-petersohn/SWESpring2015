0edb44a097f1e78844c95625d108ae1b329a0480
Devin Petersohn <dpvx8@mail.missouri.edu>
2015-05-06 18:58:26 -0500

    moar bug

--- php/application.php
+++ php/application.php
@@ -58,8 +58,8 @@
 		$wish_courses = $_POST['courseLike'];
 		$grades = $_POST['grade'];
 
-		pg_prepare($db, "q10", 'INSERT INTO applicant_prev_taught_course (sso, course, semester_taught) 
-			VALUES ($1, $2, $3);');
+		pg_prepare($db, "q10", 'INSERT INTO applicant_prev_taught_course (sso, course) 
+			VALUES ($1, $2);');
 		pg_execute($db, "q10", array($sso, $_POST['precourse']));
 		
 		pg_prepare($db, "q11", 'INSERT INTO applicant_other_workpalces (sso, workplace) VALUES ($1, $2)');
