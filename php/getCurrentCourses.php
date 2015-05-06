23095d977221f285dbcc160fa7eb29232fa210ac
Devin Petersohn <dpvx8@mail.missouri.edu>
2015-05-06 18:42:23 -0500

    bugs

--- php/getCurrentCourses.php
+++ php/getCurrentCourses.php
@@ -4,7 +4,7 @@
 	$db = db_connect();
 	
 	$course = pg_query($db, "SELECT course_id FROM course WHERE slots_available > 0;");
-		echo "<select name='selectCourses' id='courseLike'".$_POST['iCnt']." value='selection' >";
+		echo "<select name='selectCourses' id='courseLike".$_POST['iCnt']."' value='selection' >";
 		while ($printedCourse = pg_fetch_row($course)) {
 		    echo "<option value = '".$printedCourse[0]."'>".$printedCourse[0]."</option>";
 		}
