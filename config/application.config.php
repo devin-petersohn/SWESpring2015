aa8a4b8ad78f0ff7fbf05c5e7bec9b76059924d1
Madelyn Petersohn <mcs526@mail.missouri.edu>
2015-05-06 15:39:10 -0500

    change error page

--- config/application.config.php
+++ config/application.config.php
@@ -2,7 +2,7 @@
 return array(
     // This should be an array of module namespaces used in the application.
     'modules' => array(
-        'Application','Applicationform','Homepage','Professorpage','Admin','Login','Studentpage','Test','Comments','Applicationstatus','Error'
+        'Application','Applicationform','Homepage','Professorpage','Admin','Login','Studentpage','Test','Comments','Applicationstatus','error'
     ),
 
     // These are various options for the listeners attached to the ModuleManager
