2aeeb34c9183dedddf7dc3365da460c2059ccebf
Devin Petersohn <dpvx8@mail.missouri.edu>
2015-05-05 19:50:29 -0500

    Backed for adding instructor to course

---
+++ php/add_instructor_teaches.php
@@ -0,0 +1,33 @@
+<?php
+include 'functions.php';
+
+$db = db_connect();
+
+function course_dropdown() {	
+	$db = db_connect();
+	$course = pg_query($db, "SELECT course_id FROM course WHERE slots_available > 0;");
+		echo "<select name='selectCourses' id='selection' value='selection' >";
+		while ($printedCourse = pg_fetch_row($course)) {
+		    echo "<option value = '".$printedCourse[0]."'>".$printedCourse[0]."</option>";
+		}
+		echo "<\select>";
+}
+
+function instructor_dropdown() {
+	$db = db_connect();
+	$instructor = pg_query($db, "SELECT sso FROM instructor;");
+		echo "<select name='selectInstructors' id='selection' value='selection' >";
+		while ($printedInstructor = pg_fetch_row($instructor)) {
+		    echo "<option value = '".$printedInstructor[0]."'>".$printedInstructor[0]."</option>";
+		}
+		echo "<\select>";
+}
+
+
+function add_instructor_teaches($sso, $course_id) {
+    $db = db_connect();
+    pg_prepare($db, "q1", "INSERT INTO instructor_teaches (instructor_sso, course_id) VALUES ($1,$2)");
+    pg_execute($db, "q1", array($sso, $course_id));
+}
+
+?>
