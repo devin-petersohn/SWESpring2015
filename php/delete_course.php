d39b2636658e3a425a6c52082a54a51e8ec24cbf
Daniel Raymon <djry35@mail.missouri.edu>
2015-05-07 13:12:21 -0500

    some changes and catches

--- php/delete_course.php
+++ php/delete_course.php
@@ -3,9 +3,21 @@
 include 'functions.php';
 
 function deleteCourse($courseID){
-	$db = db_connect();
-	pg_prepare($db, "q1", "DELETE FROM Course WHERE course_id = $1");
-	pg_execute($db, "q1", array($courseID));
+    $db = db_connect();
+    pg_prepare($db, "q0", "SELECT * from course") or die(header("Location:Error"));
+    $SR = pg_execute($db, "q0", array()) or die(header("Location:Error"));
+    while($SR2 = pg_fetch_array($SR, null, PGSQL_ASSOC))
+    {
+        if(strcmp($SR2, $course_id) == 0)
+        {
+            pg_prepare($db, "q1", "DELETE FROM course WHERE course_id = $1") or die(header("Location:Error"));
+	        pg_execute($db, "q1", array($courseID)) or die(header("Location:Error"));
+            return;
+        }
+    }
+    
+	echo "<p style='color:red;'>Error: Course does not exists</p>";
+    return;
 }
 
 
--- php/delete_instructor.php
+++ php/delete_instructor.php
@@ -4,8 +4,20 @@ include 'functions.php';
 
 function deleteInstructor($sso){
 	$db = db_connect();
-	pg_prepare($db, "q5", "DELETE FROM Faculty WHERE sso = $1");
-	pg_execute($db, "q5", array($sso));
+	pg_prepare($db, "q0", "SELECT * from course") or die(header("Location:Error"));
+	$SR = pg_execute($db, "q0", array()) or die(header("Location:Error"));
+	while($SR2 = pg_fetch_array($SR, null, PGSQL_ASSOC))
+	{
+	    if(strcmp($SR2, $course_id) == 0)
+	    {
+	        pg_prepare($db, "q5", "DELETE FROM Faculty WHERE sso = $1") or die(header("Location:Error"));
+	        pg_execute($db, "q5", array($sso)) or die(header("Location:Error"));
+	        return;
+	    }
+	}
+	
+	echo "<p style='color:red;'>Error: Instructor does not exists</p>";
+	return;
 }
 
 ?>
