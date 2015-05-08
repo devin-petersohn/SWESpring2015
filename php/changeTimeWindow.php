b0257bcdc722fde4f2b2423782db290941c0e68e
Madelyn Petersohn <mcs526@mail.missouri.edu>
2015-05-08 09:53:26 -0500

    typo

--- php/changeTimeWindow.php
+++ php/changeTimeWindow.php
@@ -5,10 +5,10 @@ $db = db_connect();
 
 function get_timeWindow() {
     $db = db_connect();
-    $course = pg_query($db, "SELECT windowframe FROM time_window;") or die(header("Location:Error"));
+    $course = pg_query($db, "SELECT windowname FROM time_window;") or die(header("Location:Error"));
     echo "<select name='selectWindow' id='selection' value='selection' >";
     echo"<value='ERROR' disabled='disabled' seclected='selected'> Please select the timeframe to change</option>";
-    while ($printedWindow = pg_fetch_row($windowframe)) {
+    while ($printedWindow = pg_fetch_row($windowname)) {
         echo "<option value = '".$printedWindow[0]."'>".$printedWindow[0]."</option>";
     }
     echo "<\select>";
