04c85a96920f3c677c6b36584145bb285ee159b5
Madelyn Petersohn <mcs526@mail.missouri.edu>
2015-05-08 09:24:24 -0500

    making change time window module

---
+++ php/changeTimeWindow.php
@@ -0,0 +1,23 @@
+<?php
+if((include 'functions.php') != 1) header("Location:Error");
+
+$db = db_connect();
+
+function get_timeWindow() {
+    $db = db_connect();
+    $course = pg_query($db, "SELECT windowframe FROM time_window;") or die(header("Location:Error"));
+    echo "<select name='selectWindow' id='selection' value='selection' >";
+    echo"<value='ERROR' disabled='disabled' seclected='selected'> Please select the timeframe to change</option>";
+    while ($printedWindow = pg_fetch_row($windowframe)) {
+        echo "<option value = '".$printedWindow[0]."'>".$printedWindow[0]."</option>";
+    }
+    echo "<\select>";
+}
+
+
+
+
+
+
+
+?>
