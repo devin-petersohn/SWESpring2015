60dd6cc7192d08e93e1781278aec0ef4efe40ccf
zzff8 <zzff8@mail.missouri.edu>
2015-05-08 15:41:14 -0500

    hi

--- php/getcurrenttime.php
+++ php/getcurrenttime.php
@@ -4,7 +4,7 @@
 	$db = db_connect();
 	
 	$course = pg_query($db, "SELECT endtime from time_window;;");
-		echo $course[2];
+		var_dump($course);
 // 		while ($printedCourse = pg_fetch_row($course)) {
 // 		    echo "<option value = '".$printedCourse[0]."'>".$printedCourse[0]."</option>";
 // 		}
